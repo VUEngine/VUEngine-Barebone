9b01e0221a9fcc2d1a716577a111db1ecb1dd80a
Chris <chris@vr32.de>
2015-11-17 19:27:11 +0100

    Updated GPL license to v3

--- assets/languages/Language_FR.c
+++ assets/languages/Language_FR.c
@@ -1,21 +1,17 @@
 /* VBJaEngine: bitmap graphics engine for the Nintendo Virtual Boy
  *
- * Copyright (C) 2007 Jorge Eremiev
- * jorgech3@gmail.com
+ * Copyright (C) 2007 Jorge Eremiev <jorgech3@gmail.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ * You should have received a copy of the GNU General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
  */
 
 
@@ -38,24 +34,24 @@
 const LangROMDef LANGUAGE_FR =
 {
     // Language Name
-    "Français",
+    "Franï¿½ais",
 
     // Strings
     {
         //STR_PRECAUTION_SCREEN_TITLE:
         "IMPORTANT:",
         //STR_PRECAUTION_SCREEN_TEXT:
-        "PRENEZ CONAISSANCE DE LA\n\nNOTICE ET DES PRÉCAUTIONS\n\nA PRENDRE AVANT DE JOUER",
+        "PRENEZ CONAISSANCE DE LA\n\nNOTICE ET DES PRï¿½CAUTIONS\n\nA PRENDRE AVANT DE JOUER",
         //STR_AUTOMATIC_PAUSE:
         "Pause Automatique",
         //STR_AUTOMATIC_PAUSE_EXPLANATION:
         "  La fonction Pause automatique va\nvous rappelerez de prendre une pause\n  de jeu chaque 30 minutes environ",
         //STR_AUTOMATIC_PAUSE_TEXT:
-        "Se il vous plaît, prendre un repos!",
+        "Se il vous plaï¿½t, prendre un repos!",
         //STR_ON:
-        "Activé",
+        "Activï¿½",
         //STR_OFF:
-        "Desactivé",
+        "Desactivï¿½",
         //STR_LANGUAGE_SELECT:
         "Choix de la langue",
 
