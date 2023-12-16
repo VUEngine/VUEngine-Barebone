dc32adc716fd65b18f67a21fa1c15eed56eb83be
Christian Radke <c.radke@posteo.de>
2023-12-16 13:34:17 +0100

    Update project structure

---
+++ source/States/MyGameScreenState/MyGameState.h
@@ -0,0 +1,34 @@
+/**
+ * VUEngine Barebone
+ *
+ * © Jorge Eremiev <jorgech3@gmail.com> and Christian Radke <c.radke@posteo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+#ifndef MY_GAME_STATE_H_
+#define MY_GAME_STATE_H_
+
+
+//---------------------------------------------------------------------------------------------------------
+// 												INCLUDES
+//---------------------------------------------------------------------------------------------------------
+
+#include <GameState.h>
+
+
+//---------------------------------------------------------------------------------------------------------
+// 											CLASS'S DECLARATION
+//---------------------------------------------------------------------------------------------------------
+
+dynamic_singleton class MyGameState : GameState
+{
+	static MyGameState getInstance();
+	override void enter(void* owner);
+	override void suspend(void* owner);
+	override void resume(void* owner);
+}
+
+
+#endif
