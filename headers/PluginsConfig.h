6dda332c5cd6f352bba5b80a081f9c3e3e4c1ddf
Jorge Eremiev <jorgech3@gmail.com>
2024-03-30 00:22:20 -0500

    Catching up.

--- headers/PluginsConfig.h
+++ headers/PluginsConfig.h
@@ -1,30 +1,48 @@
-/**
- * VUEngine Barebone
- *
- * © Jorge Eremiev <jorgech3@gmail.com> and Christian Radke <c.radke@posteo.de>
- *
- * For the full copyright and license information, please view the LICENSE file
- * that was distributed with this source code.
- */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                              THIS FILE WAS AUTO-GENERATED - DO NOT EDIT                               //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef PLUGINS_CONFIG_H_
 #define PLUGINS_CONFIG_H_
 
 
 //---------------------------------------------------------------------------------------------------------
-//												PLUGINS
+//                                          CONFIGURATION MACROS                                           
 //---------------------------------------------------------------------------------------------------------
 
-#define __SAVE_DATA_MANAGER_SAVE_STAMP						"VUEngine"
-#define __SAVE_DATA_MANAGER_SAVE_STAMP_LENGTH				8
+#define __PLUGIN_AUTOMATIC_PAUSE_TITLE_TEXT_FONT ("Default")
+#define __PLUGIN_AUTOMATIC_PAUSE_BODY_TEXT_FONT ("Default")
+#define __PLUGIN_AUTOMATIC_PAUSE_USE_LOW_POWER_ENTITY (false)
 
-#define __ADJUSTMENT_SCREEN_USE_LOW_POWER_ENTITY			true
-#define __LANGUAGE_SELECTION_SCREEN_VARIANT					1
+#define __PLUGIN_SAVE_DATA_MANAGER_CRC_MASK (0xEDB88320)
+#define __PLUGIN_SAVE_DATA_MANAGER_CRC_CHECK_RANGE (512)
+#define __PLUGIN_SAVE_DATA_MANAGER_SAVE_STAMP ("VUEngine")
+#define __PLUGIN_SAVE_DATA_MANAGER_SAVE_STAMP_LENGTH (8)
 
-#define __AUTOMATIC_PAUSE_SCREEN_TITLE_TEXT_FONT			"AstonishExt"
-#define __AUTOMATIC_PAUSE_SELECTION_SCREEN_TITLE_TEXT_FONT	"AstonishExt"
-#define __LANGUAGE_SELECTION_SCREEN_TITLE_TEXT_FONT 		"AstonishExt"
-#define __PRECAUTION_SCREEN_TEXT_FONT						"AstonishExt"
+#define __PLUGIN_ADJUSTMENT_SCREEN_VUENGINE_USE_LOW_POWER_ENTITY (false)
+#define __PLUGIN_ADJUSTMENT_SCREEN_VUENGINE_LOW_POWER_INDICATOR_X_POSITION (4)
+#define __PLUGIN_ADJUSTMENT_SCREEN_VUENGINE_LOW_POWER_INDICATOR_Y_POSITION (1)
+#define __PLUGIN_ADJUSTMENT_SCREEN_VUENGINE_LOW_POWER_ENTITY_X_POSITION (-152)
+#define __PLUGIN_ADJUSTMENT_SCREEN_VUENGINE_LOW_POWER_ENTITY_Y_POSITION (-100)
+#define __PLUGIN_ADJUSTMENT_SCREEN_VUENGINE_LOW_POWER_ENTITY_Z_POSITION (0)
+#define __PLUGIN_ADJUSTMENT_SCREEN_VUENGINE_LOW_POWER_ENTITY_Z_DISPLACEMENT (0)
+
+#define __PLUGIN_AUTOMATIC_PAUSE_SELECTION_SCREEN_USE_LOW_POWER_ENTITY (false)
+#define __PLUGIN_AUTOMATIC_PAUSE_SELECTION_SCREEN_OPTIONS_GAP (3)
+#define __PLUGIN_AUTOMATIC_PAUSE_SELECTION_SCREEN_OPTIONS_Y_POS (15)
+#define __PLUGIN_AUTOMATIC_PAUSE_SELECTION_SCREEN_TITLE_TEXT_FONT ("Default")
+#define __PLUGIN_AUTOMATIC_PAUSE_SELECTION_SCREEN_BODY_TEXT_FONT ("Default")
+#define __PLUGIN_AUTOMATIC_PAUSE_SELECTION_SCREEN_OPTIONS_TEXT_FONT ("Default")
+
+#define __PLUGIN_LANGUAGE_SELECTION_SCREEN_USE_LOW_POWER_ENTITY (false)
+#define __PLUGIN_LANGUAGE_SELECTION_SCREEN_VARIANT (0)
+#define __PLUGIN_LANGUAGE_SELECTION_SCREEN_TITLE_TEXT_FONT ("Default")
+#define __PLUGIN_LANGUAGE_SELECTION_SCREEN_LANGUAGE_NAME_FONT ("Default")
+#define __PLUGIN_LANGUAGE_SELECTION_SCREEN_IMAGE_WIDTH (44)
+#define __PLUGIN_LANGUAGE_SELECTION_SCREEN_IMAGE_HEIGHT (32)
+
+#define __PLUGIN_PRECAUTION_SCREEN_USE_LOW_POWER_ENTITY (false)
+#define __PLUGIN_PRECAUTION_SCREEN_TEXT_FONT ("Default")
 
 
 #endif
