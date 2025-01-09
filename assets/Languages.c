fffa57fd76d2fdccc9fac551cc2a5af3676cdec3
Jorge Eremiev <jorgech3@gmail.com>
2025-01-09 16:12:20 -0500

    Indentation fixes.

--- assets/Languages.c
+++ assets/Languages.c
@@ -1,6 +1,7 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                              THIS FILE WAS AUTO-GENERATED - DO NOT EDIT                               //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////THIS FILE WAS AUTO-GENERATED - DO NOT EDIT //////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // INCLUDES
@@ -35,24 +36,24 @@ LangROMSpec* const _languages[] =
 
 const char* const EnglishLanguageStrings[] =
 {
-    /* kStringAutomaticPauseBody: */
-    "Please take a rest!",
-    /* kStringAutomaticPauseSelectionBody: */
-    "The Automatic Pause feature will\nremind you to take a break from\nplaying approx. every 30 minutes",
-    /* kStringAutomaticPauseSelectionOff: */
-    "Off",
-    /* kStringAutomaticPauseSelectionOn: */
-    "On",
-    /* kStringAutomaticPauseSelectionTitle: */
-    "AUTOMATIC PAUSE",
-    /* kStringAutomaticPauseTitle: */
-    "AUTOMATIC PAUSE",
-    /* kStringLanguageSelectTitle: */
-    "LANGUAGE SELECT",
-    /* kStringPrecautionScreenBody: */
-    "     IMPORTANT\nREAD INSTRUCTION AND\nPRECAUTION BOOKLETS\n  BEFORE OPERATING",
-    /* kStringYourGameHere: */
-    "Your Game Here",
+	/* kStringAutomaticPauseBody: */
+	"Please take a rest!",
+	/* kStringAutomaticPauseSelectionBody: */
+	"The Automatic Pause feature will\nremind you to take a break from\nplaying approx. every 30 minutes",
+	/* kStringAutomaticPauseSelectionOff: */
+	"Off",
+	/* kStringAutomaticPauseSelectionOn: */
+	"On",
+	/* kStringAutomaticPauseSelectionTitle: */
+	"AUTOMATIC PAUSE",
+	/* kStringAutomaticPauseTitle: */
+	"AUTOMATIC PAUSE",
+	/* kStringLanguageSelectTitle: */
+	"LANGUAGE SELECT",
+	/* kStringPrecautionScreenBody: */
+	"     IMPORTANT\nREAD INSTRUCTION AND\nPRECAUTION BOOKLETS\n  BEFORE OPERATING",
+	/* kStringYourGameHere: */
+	"Your Game Here",
 };
 
 const LangROMSpec EnglishLanguageSpec =
@@ -69,24 +70,24 @@ const LangROMSpec EnglishLanguageSpec =
 
 const char* const GermanLanguageStrings[] =
 {
-    /* kStringAutomaticPauseBody: */
-    "Bitte mache eine Pause!",
-    /* kStringAutomaticPauseSelectionBody: */
-    " Die Automatische Pause Funktion\nerinnert dich daran, das Spiel ca.\n  alle 30 Minuten zu pausieren",
-    /* kStringAutomaticPauseSelectionOff: */
-    "Aus",
-    /* kStringAutomaticPauseSelectionOn: */
-    "An",
-    /* kStringAutomaticPauseSelectionTitle: */
-    "AUTOMATISCHE PAUSE",
-    /* kStringAutomaticPauseTitle: */
-    "AUTOMATISCHE PAUSE",
-    /* kStringLanguageSelectTitle: */
-    "SPRACHAUSWAHL",
-    /* kStringPrecautionScreenBody: */
-    "        WICHTIG\nBITTE LESEN SIE VOR DEM\n SPIELEN DIE ANLEITUNG\nUND SICHERHEITSHINWEISE",
-    /* kStringYourGameHere: */
-    "Dein Spiel hier",
+	/* kStringAutomaticPauseBody: */
+	"Bitte mache eine Pause!",
+	/* kStringAutomaticPauseSelectionBody: */
+	" Die Automatische Pause Funktion\nerinnert dich daran, das Spiel ca.\n  alle 30 Minuten zu pausieren",
+	/* kStringAutomaticPauseSelectionOff: */
+	"Aus",
+	/* kStringAutomaticPauseSelectionOn: */
+	"An",
+	/* kStringAutomaticPauseSelectionTitle: */
+	"AUTOMATISCHE PAUSE",
+	/* kStringAutomaticPauseTitle: */
+	"AUTOMATISCHE PAUSE",
+	/* kStringLanguageSelectTitle: */
+	"SPRACHAUSWAHL",
+	/* kStringPrecautionScreenBody: */
+	"        WICHTIG\nBITTE LESEN SIE VOR DEM\n SPIELEN DIE ANLEITUNG\nUND SICHERHEITSHINWEISE",
+	/* kStringYourGameHere: */
+	"Dein Spiel hier",
 };
 
 const LangROMSpec GermanLanguageSpec =
@@ -103,24 +104,24 @@ const LangROMSpec GermanLanguageSpec =
 
 const char* const SpanishLanguageStrings[] =
 {
-    /* kStringAutomaticPauseBody: */
-    "�Por favor, t�mese un descanso!",
-    /* kStringAutomaticPauseSelectionBody: */
-    " La funci�n autom�tica de pausa te\nrecordar� que debes tomar un descanso\n  approximadamente cada 30 minutos",
-    /* kStringAutomaticPauseSelectionOff: */
-    "Apagado",
-    /* kStringAutomaticPauseSelectionOn: */
-    "Encendido",
-    /* kStringAutomaticPauseSelectionTitle: */
-    "PAUSA AUTOM�TICA",
-    /* kStringAutomaticPauseTitle: */
-    "PAUSA AUTOM�TICA",
-    /* kStringLanguageSelectTitle: */
-    "SELECCIONAR IDIOMA",
-    /* kStringPrecautionScreenBody: */
-    "         IMPORTANTE\n     LEE LOS LIBROS DE\nINSTRUCCIONES Y PRECAUCIONES\n       ANTES DE JUGAR",
-    /* kStringYourGameHere: */
-    "Su juego aqu�",
+	/* kStringAutomaticPauseBody: */
+	"�Por favor, t�mese un descanso!",
+	/* kStringAutomaticPauseSelectionBody: */
+	" La funci�n autom�tica de pausa te\nrecordar� que debes tomar un descanso\n  approximadamente cada 30 minutos",
+	/* kStringAutomaticPauseSelectionOff: */
+	"Apagado",
+	/* kStringAutomaticPauseSelectionOn: */
+	"Encendido",
+	/* kStringAutomaticPauseSelectionTitle: */
+	"PAUSA AUTOM�TICA",
+	/* kStringAutomaticPauseTitle: */
+	"PAUSA AUTOM�TICA",
+	/* kStringLanguageSelectTitle: */
+	"SELECCIONAR IDIOMA",
+	/* kStringPrecautionScreenBody: */
+	"         IMPORTANTE\n     LEE LOS LIBROS DE\nINSTRUCCIONES Y PRECAUCIONES\n       ANTES DE JUGAR",
+	/* kStringYourGameHere: */
+	"Su juego aqu�",
 };
 
 const LangROMSpec SpanishLanguageSpec =
