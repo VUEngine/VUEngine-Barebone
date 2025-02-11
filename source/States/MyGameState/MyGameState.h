bdbed3c7032f07edbda20d0993314e25b4896730
Christian Radke <c.radke@posteo.de>
2025-02-11 18:52:04 +0100

    Move AlignmentCheckScreen button sequence checks to plugin

--- source/States/MyGameState/MyGameState.h
+++ source/States/MyGameState/MyGameState.h
@@ -10,30 +10,25 @@
 #ifndef MY_GAME_STATE_H_
 #define MY_GAME_STATE_H_
 
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // INCLUDES
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-#include <AlignmentCheckScreenState.h>
+#include <AlignmentCheckBaseGameState.h>
 #include <GameState.h>
 
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // CLASS' DECLARATION
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 ///
 /// Class MyGameState
 ///
 /// Inherits from GameState
 ///
-/// Implements an empty game state intended to be the starting for a new, awesome game.
-singleton class MyGameState : GameState
+/// Implements an empty game state intended to be the starting point for a new, awesome game.
+singleton class MyGameState : AlignmentCheckBaseGameState
 {
-	/// @protectedsection
-
-	/// Cache of user's inputs
-	uint32 lastInputs[__PLUGIN_ALIGNMENT_CHECK_BUTTON_SEQUENCE_LENGTH];
-
 	/// @publicsection
 
 	/// Method to get the singleton instance
@@ -51,10 +46,6 @@ singleton class MyGameState : GameState
 	/// Prepares the object to become active in this state.
 	/// @param owner: Object that is in this state
 	override void resume(void* owner);
-
-	/// Process the provided user input.
-	/// @param userInput: Struct with the current user input information
-	override void processUserInput(const UserInput* userInput);
 }
 
 #endif
